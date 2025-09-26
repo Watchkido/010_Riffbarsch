e5c448c748b62dcbc14941404018107509f88ed1
Frank Albrecht <44406884+Watchkido@users.noreply.github.com>
2025-09-26 11:13:37 +0200

    infos gelöscht

--- tests/test_10_Umwelt.py
+++
@@ -1,8 +0,0 @@
-"""
-test_10_Umwelt.py
-Umwelttests für verschiedene Systemumgebungen.
-Hier wird das Verhalten unter verschiedenen Bedingungen getestet.
-"""
-
-def test_environment():
-    assert True
